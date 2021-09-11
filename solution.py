36d45a75b300f251be99abebb8f31fac793aa2ca
unknown <popuriyashwanth@gmail.com>
2021-09-11 17:48:40 -0500

    change in answers

--- solution.py
+++ solution.py
@@ -8,13 +8,13 @@ def welcome_assignment_answers(question):
     qa = {
         "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?": "mTLS",
         "Are encoding and encryption the same? - Yes/No": "No",
-        "Is it possible to decrypt a message without a key? - Yes / No": "No",
+        "Is it possible to decrypt a message without a key? - Yes / No": "Yes",
         "Is it possible to decode a message without a key? - Yes/No": "Yes",
         "Is a hashed message supposed to be un-hashed? - Yes/No": "No",
         "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code": "42b76fe51778764973077a5a94056724",
-         "Is MD5 a secured hashing algorithm? - Yes/No": "Yes",
-        "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number": 7,
-        "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":7
+         "Is MD5 a secured hashing algorithm? - Yes/No": "No",
+        "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number": 5,
+        "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":4
 
     }
 
